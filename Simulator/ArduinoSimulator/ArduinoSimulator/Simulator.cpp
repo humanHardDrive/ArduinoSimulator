ee93241fa0b9f2d7b54e8299505fb8a3c8697572
Sam Stratter <humanHardDrive@gmail.com>
2018-03-25 21:49:24 -0400

    Updated Read/Write methods
    Something has already declared INPUT. Need to fix this.

--- Simulator/ArduinoSimulator/ArduinoSimulator/Simulator.cpp
+++ Simulator/ArduinoSimulator/ArduinoSimulator/Simulator.cpp
@@ -1,19 +1,41 @@
 #include "Simulator.h"
 
+PIN_MODE l_CurrentPinMode[PIN_COUNT];
+PIN_STATE l_CurrentPinState[PIN_COUNT];
 
+unsigned int l_SystemMilliSecClk = 0;
 
 void pinMode(uint8_t pin, PIN_MODE mode)
 {
 
 }
 
-void digitalRead(uint8_t pin)
+
+PIN_STATE digitalRead(uint8_t pin)
+{
+	return l_CurrentPinState[pin];
+}
+
+void digitalWrite(uint8_t pin, PIN_STATE state)
 {
 
 }
 
 
-void digitalWrite(uint8_t pin)
+DWORD WINAPI IncrementSystemMilliSecsClk(LPVOID lpParam)
 {
+	l_SystemMilliSecClk++;
+	return 0;
+}
 
+unsigned int millis()
+{
+	return l_SystemMilliSecClk;
+}
+
+void delay(unsigned int ms)
+{
+	unsigned int CurrentTime = millis();
+
+	while ((CurrentTime + ms) > millis());
 }
--- Simulator/ArduinoSimulator/ArduinoSimulator/Simulator.h
+++ Simulator/ArduinoSimulator/ArduinoSimulator/Simulator.h
@@ -1,11 +1,23 @@
 #pragma once
 #include <cstdint>
 
-enum PIN_MODE
+#include <Windows.h>
+
+#include "Serial.h"
+
+#define PIN_COUNT	20
+
+typedef enum
 {
-	INPUT = 0,
+	INPUT1 = 0, //Input is defined by something else?
 	OUTPUT,
 	INPUT_PULLUP
+}PIN_MODE;
+
+enum PIN_STATE
+{
+	LOW = 0,
+	HIGH
 };
 
 void setup();
@@ -13,5 +25,9 @@ void loop();
 
 void pinMode(uint8_t pin, PIN_MODE mode);
 
-void digitalRead(uint8_t pin);
-void digitalWrite(uint8_t pin);
+PIN_STATE digitalRead(uint8_t pin);
+void digitalWrite(uint8_t pin, PIN_STATE state);
+
+DWORD WINAPI IncrementSystemMilliSecsClk(LPVOID lpParam);
+unsigned int millis();
+void delay(unsigned int ms);
