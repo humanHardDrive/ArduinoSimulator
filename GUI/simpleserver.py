7c340d20a67586c0129c1706c08187ff69be0f26
Sam Stratter <humanHardDrive@gmail.com>
2018-03-31 13:29:39 -0400

    Added config file
    This file will handle how all the hardware connects and all the devices
    to manage.

--- GUI/simpleserver.py
+++ GUI/simpleserver.py
@@ -3,13 +3,17 @@ import socket
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
-BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
+BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
-s.close()
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
 
-print "received data:", data
+conn, addr = s.accept()
+print 'Connection address:', addr
+while 1:
+    data = conn.recv(BUFFER_SIZE)
+    if not data: break
+    print "received data:", data
+    conn.send(data)  # echo
+conn.close()
