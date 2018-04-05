e52d874b2854fad6e25b189478afdd8adaccf7f1
Sam Stratter <humanHardDrive@gmail.com>
2018-04-04 20:02:53 -0400

    Updated connection class

--- Simulator/ArduinoSimulator/ArduinoSimulator/main.cpp
+++ Simulator/ArduinoSimulator/ArduinoSimulator/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <cstdlib>
 
-#include "Simulator.h"
-#include "Logging.h"
+//#include "Simulator.h"
+//#include "Logging.h"
+
+#include "Connection.h"
 
 #define VERSION_MINOR	0
 #define VERSION_MAJOR	0
@@ -12,18 +14,27 @@ int main(int argc, char** argv)
 {
 	HANDLE SystemMilliSecClkInterrupt;
 	bool stopRunning = false;
+	bool connected = false;
 
 	std::cout << "Arduino Simulator v" << VERSION_MAJOR << "." << VERSION_MINOR << std::endl;
-	DISPLAY_LOG(LVL_INFO, "Logging Started. Execution Started");
+	//DISPLAY_LOG(LVL_INFO, "Logging Started. Execution Started");
 
-	SystemMilliSecClkInterrupt = CreateThread(NULL, 0, IncrementSystemMilliSecsClk, NULL, 0, NULL);
+	//SystemMilliSecClkInterrupt = CreateThread(NULL, 0, IncrementSystemMilliSecsClk, NULL, 0, NULL);
 
-	setup();
+	Connection c("127.0.0.1", "4040");
+	connected = c.start();
+
+	//setup();
 	while (!stopRunning)
 	{
-		loop();
+		if (connected)
+			c.write("HELLO WORLD");
+
+		//loop();
 		stopRunning = true;
 	}
+
+	c.stop();
 
 	return 1;
 }
