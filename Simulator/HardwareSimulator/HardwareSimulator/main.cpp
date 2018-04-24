73f72d9cf2389a0ae6b23b055747f7f03878be0a
humanharddrive <humanharddrive@gmail.com>
2018-04-24 19:18:02 -0400

    Start bringing in python to do interperting.
    Need to make the path to python libraries customizable

--- Simulator/HardwareSimulator/HardwareSimulator/main.cpp
+++ Simulator/HardwareSimulator/HardwareSimulator/main.cpp
@@ -5,6 +5,8 @@
 #include "Connection.h"
 #include "Server.h"
 
+#include "Python.h"
+
 static void msghandler(char* msg, size_t size, Client* c)
 {
 	std::string smsg(msg, size);
@@ -14,22 +16,12 @@ static void msghandler(char* msg, size_t size, Client* c)
 
 int main(int argc, char** argv)
 {
-	//Connection c("127.0.0.1", "8080");
-	Server s("127.0.0.1", "4040", 10);
-	s.SetMsgHandler(msghandler);
+	Server Arduino0("127.0.0.1", "4040", 10);
+	Arduino0.SetMsgHandler(msghandler);
 
-	/*if (c.start())
-	{
-		for (int i = 0; i < 10; i++)
-		{
-			using namespace std::chrono_literals;
-			c.write("{\"pin\":\"10\", \"type\":\"serial\", \"baud\":\"9600\", \"data\":\"AB\"}");
-			std::this_thread::sleep_for(2s);
-		}
-		c.stop();
-	}*/
+	Py_Initialize();
 
-	s.start();
+	Arduino0.start();
 	while (1);
 
 	return 0;
