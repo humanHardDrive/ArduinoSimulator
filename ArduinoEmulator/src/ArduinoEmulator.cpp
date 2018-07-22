1eb41a8eab3d0001e5b74163c5537b28d4abbc72
Sam Stratter <humanharddrive@gmail.com>
2018-07-22 12:00:42 -0400

    Handled all of the 2-operand instructions

--- ArduinoEmulator/src/ArduinoEmulator.cpp
+++ ArduinoEmulator/src/ArduinoEmulator.cpp
@@ -3,25 +3,89 @@
 
 using namespace std;
 
-#define VAR_BYTE(var, byte)		((uint8_t*)(&var)[byte])
-#define BIT_SET(var, bit)		(var & (1 << bit))
-
 void l_NOP()
 {
 	std::cout << "NOP" << std::endl;
 }
 
-//https://en.wikipedia.org/wiki/Atmel_AVR_instruction_set
-void ParseInstruction(uint16_t inst)
+//2-operand instructions
+void l_CP(uint8_t rd, uint8_t rr)
 {
-	uint8_t instcode = VAR_BYTE(inst, 3);
+	std::cout << "CP" << std::endl;
+}
+
+void l_CPC(uint8_t rd, uint8_t rr)
+{
+	std::cout << "CPC" << std::endl;
+}
+
+void l_SUB(uint8_t rd, uint8_t rr)
+{
+	std::cout << "SUB" << std::endl;
+}
+
+void l_SBC(uint8_t rd, uint8_t rr)
+{
+	std::cout << "SBC" << std::endl;
+}
+
+void l_ADD(uint8_t rd, uint8_t rr)
+{
+	std::cout << "ADD" << std::endl;
+}
+
+void l_ADC(uint8_t rd, uint8_t rr)
+{
+	std::cout << "ADC" << std::endl;
+}
+
+void l_CPSE(uint8_t rd, uint8_t rr)
+{
+	std::cout << "CPSE" << std::endl;
+}
+
+void l_AND(uint8_t rd, uint8_t rr)
+{
+	std::cout << "AND" << std::endl;
+}
+
+void l_EOR(uint8_t rd, uint8_t rr)
+{
+	std::cout << "EOR" << std::endl;
+}
+
+void l_OR(uint8_t rd, uint8_t rr)
+{
+	std::cout << "OR" << std::endl;
+}
+
+void l_MOV(uint8_t rd, uint8_t rr)
+{
+	std::cout << "MOV" << std::endl;
+}
+
+void l_CPI(uint8_t rd, uint8_t k)
+{
+	std::cout << "CPI" << std::endl;
+}
+
+/*
+ *The opcodes for this method were found here:
+ *https://en.wikipedia.org/wiki/Atmel_AVR_instruction_set
+ *The opcodes also exist in the AVR specification, but the Wiki
+ *provides a nice table that made writing the parser much easier
+ *than organizing all of the entries from AVR's documentation.
+*/
+bool ParseInstruction(uint16_t inst)
+{
+	uint8_t instcode = ((uint8_t*)(&inst))[3];
 	uint8_t opcode = instcode;
 
 	//Easy case
 	if(!inst)
 	{
 		l_NOP();
-		return;
+		return true;
 	}
 
 	//Get the 2 most significant bits
@@ -34,6 +98,82 @@ void ParseInstruction(uint16_t inst)
 			{
 				//2 operand instructions
 				opcode = (opcode & 0x3C) >> 2;
+				switch(opcode)
+				{
+					case 1:
+						l_CPC(0, 0);
+						return true;
+						break;
+
+					case 2:
+						l_SBC(0, 0);
+						return true;
+						break;
+
+					//The Wiki entry for this opcode is wrong, at time of writing 7/22/18
+					//From the AVR documentation no part of the register is in the opcode
+					//According to the Wiki, there would be a collision between this instruction
+					//and the SBC instruction
+					case 3:
+						l_ADD(0, 0);
+						return true;
+						break;
+
+					case 4:
+						l_CPSE(0, 0);
+						return true;
+						break;
+
+					case 5:
+						l_CP(0, 0);
+						return true;
+						break;
+
+					case 6:
+						l_SUB(0,0);
+						return true;
+						break;
+
+					case 7:
+						l_ADC(0,0);
+						return true;
+						break;
+
+					case 8:
+						l_AND(0, 0);
+						return true;
+						break;
+
+					case 9:
+						l_EOR(0, 0);
+						return true;
+						break;
+
+					case 10:
+						l_OR(0, 0);
+						return true;
+						break;
+
+					case 11:
+						l_MOV(0,0);
+						return true;
+						break;
+
+					//This isn't a 2-operand instruction, but the instruction code is 0
+					//So I'll handle it here, even though part of the constant is in the opcode
+					case 12:
+					case 13:
+					case 14:
+					case 15:
+						l_CPI(0, 0);
+						return true;
+						break;
+
+					default:
+						std::cout << "UNKOWN " << inst << std::endl;
+						return false;
+						break;
+				}
 			}
 			else
 			{
@@ -51,6 +191,8 @@ void ParseInstruction(uint16_t inst)
 		case 3:
 			break;
 	}
+
+	return false;
 }
 
 int main()
